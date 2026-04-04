ba33651fa15b1b23456664b3d2057d3b75d431b5
Gautam <159130133+CookieCums@users.noreply.github.com>
2026-04-04 12:09:58 +0530

    Update logout.sh

--- logout.sh
+++ logout.sh
@@ -1,2 +1,7 @@
-rm -rf /data/data/com.pubg.imobile/databases
-rm -rf /data/data/com.pubg.imobile/shared_prefs
+nsenter --mount=/proc/1/ns/mnt -- sh -c "
+    setenforce 0
+    rm -rf /data/data/com.pubg.imobile/databases/
+    rm -rf /data/data/com.pubg.imobile/shared_prefs/
+    setenforce 1
+"
+echo "Done"
