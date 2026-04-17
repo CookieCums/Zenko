94c232684c5ea85e4a43a7a06fb4d7175bc59611
Gautam <159130133+CookieCums@users.noreply.github.com>
2026-04-18 03:33:02 +0530

    Delete logout.sh

--- logout.sh
+++
@@ -1,7 +0,0 @@
-nsenter --mount=/proc/1/ns/mnt -- sh -c "
-    setenforce 0
-    rm -rf /data/data/com.pubg.imobile/databases/
-    rm -rf /data/data/com.pubg.imobile/shared_prefs/
-    setenforce 1
-"
-echo "Done"
